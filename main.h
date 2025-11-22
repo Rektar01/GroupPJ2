a5c1fd125d8bd3b6e5794aaadb1d9326d3ae97b0
Rektar <42653010+Rektar01@users.noreply.github.com>
2025-11-22 18:25:09 +0000

    Add files via upload

---
+++ main.h
@@ -0,0 +1,54 @@
+/**
+ * @file main.h
+ * @brief header file for main program functions
+ * @author Assignment 2 Group
+ * @date 2025-11-17
+ *
+ * This header file containing function declarations for the card game
+ * main program logic.
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "Card.h"
+#include "CardDeck.h"
+
+ /**
+  * @brief Deals cards to players from the hidden deck
+  *
+  * Deals the specified number of cards to each player alternately,
+  * taking cards from the top of the hidden deck.
+  *
+  * @param hiddenDeck Pointer to the hidden deck
+  * @param player1 Pointer to player 1's deck
+  * @param player2 Pointer to player 2's deck
+  * @param cardsPerPlayer Number of cards to deal to each player
+  */
+void dealCards(CardDeck* hiddenDeck, CardDeck* player1, CardDeck* player2, int cardsPerPlayer);
+
+/**
+ * @brief Handle a player's turn
+ *
+ * Checks if the player has a matching card. If yes, play it.
+ * If not picks a card from the hidden deck.
+ *
+ * @param player Pointer to the player's deck
+ * @param hiddenDeck Pointer to the hidden deck
+ * @param playedDeck Pointer to the played deck
+ * @param playerNum Player number (1 or 2)
+ */
+void playTurn(CardDeck* player, CardDeck* hiddenDeck, CardDeck* playedDeck, int playerNum);
+
+/**
+ * @brief Refill hidden deck from played cards
+ *
+ * When the hidden deck is empty, this function transfers all played cards
+ * except the top one to the hidden deck and shuffles it.
+ *
+ * @param hiddenDeck Pointer to the hidden deck
+ * @param playedDeck Pointer to the played deck
+ */
+void refillHiddenDeck(CardDeck* hiddenDeck, CardDeck* playedDeck);
+
+#endif /* MAIN_H */
